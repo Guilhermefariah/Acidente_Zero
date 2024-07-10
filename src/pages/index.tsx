ba44f0fae6efc5d9a6f1f13c6b998d8ba6d74de5
Guilhermefariah <guilhermefaria321@gmail.com>
2024-07-10 15:45:57 -0300

    adicionando Header ao componente index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 'use client';
 import Head from "next/head";
+import Header from "@/components/Header/Header";
 
 export default function Home() {
     return (
@@ -8,6 +9,7 @@ export default function Home() {
                 <title>Acidente Zero</title>
                 <link rel="icon" href="/favicon.icon" />
             </Head>
+            <Header />
         </div>
     );
 }
