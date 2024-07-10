90c61d332798b6219e5d649aefaabf48fd4d3bb0
Guilhermefariah <guilhermefaria321@gmail.com>
2024-07-10 15:26:12 -0300

    adicionando Head ao componente index

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+'use client';
+import Head from "next/head";
+
+export default function Home() {
+    return (
+        <div>
+            <Head>
+                <title>Acidente Zero</title>
+                <link rel="icon" href="/favicon.icon" />
+            </Head>
+        </div>
+    );
+}
