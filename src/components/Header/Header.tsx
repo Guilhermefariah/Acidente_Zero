988b15dc4dea3ef0342a587231648bc9c5bd9fe6
Guilhermefariah <guilhermefaria321@gmail.com>
2024-07-10 18:08:50 -0300

    import css

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import styles from './styles/Header.module.css';
+
 
 const Header: React.FC = () => {
     return (
-        <header>
-            a
+        <header className={styles.header}>
+            <div>
+                <div>
+                    a
+                </div>
+            </div>
         </header>
     )
 }
