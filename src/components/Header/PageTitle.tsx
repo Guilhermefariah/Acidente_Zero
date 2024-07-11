7f205cb46c9d363b320a5114cb8f53b59876cb5b
Guilhermefariah <guilhermefaria321@gmail.com>
2024-07-11 14:07:02 -0300

    update title

--- src/components/Header/PageTitle.tsx
+++ src/components/Header/PageTitle.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 interface PageTitleProps {
     title: string;
@@ -6,8 +7,15 @@ interface PageTitleProps {
 
 const PageTitle: React.FC<PageTitleProps> = ({ title }) => {
     return (
-        <h1 className="text-xl font-serif ml-4">{ title }</h1>
-    )
+        <motion.h1
+            className="text-4xl text-gray-600 hover:text-gray-900 font-serif ml-4"
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+        >
+            {title}
+        </motion.h1>
+    );
 }
 
 export default PageTitle;
