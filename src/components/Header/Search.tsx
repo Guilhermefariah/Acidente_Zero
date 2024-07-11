9b07d1c129490c9d4a73636d0dce734c1d7e9999
Guilhermefariah <guilhermefaria321@gmail.com>
2024-07-11 16:44:51 -0300

    UI search

--- src/components/Header/Search.tsx
+++ src/components/Header/Search.tsx
@@ -16,12 +16,12 @@ const Search: React.FC = () => {
                 type="text"
                 placeholder="Pesquisar..."
                 ref={inputRef}
-                className="px-3 py-2 pl-10 border border-gray-200 font-serif rounded-md focus:outline-none focus:border-gray-600 w-full md:w-auto"
+                className="px-2 py-2 pl-10 border text-gray-600 border-gray-200 font-serif rounded-md focus:outline-none focus:border-gray-400 w-full md:w-auto"
             />
             <button 
                 type="button"
                 onClick={iconClick}
-                className="absolute left-0 top-0 h-full flex items-center pl-2 text-gray-600"
+                className="absolute left-0 top-0 h-full flex items-center pl-2 text-gray-900"
             >
                 <FiSearch size={20}/>
             </button>
