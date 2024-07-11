0562457c3425c69b1f77f0144ebdfbf2510c7b03
Guilhermefariah <guilhermefaria321@gmail.com>
2024-07-11 14:29:22 -0300

    focus icon

--- src/components/Header/Search.tsx
+++ src/components/Header/Search.tsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useRef } from "react";
 import { FiSearch } from "react-icons/fi";
 
 const Search: React.FC = () => {
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const iconClick = () => {
+        if (inputRef.current) {
+            inputRef.current.focus();
+        }
+    };
+
     return (
         <div className="relative flex items-center">
             <input 
                 type="text"
                 placeholder="Pesquisar..."
+                ref={inputRef}
                 className="px-3 py-2 pl-10 border border-gray-200 font-serif rounded-md focus:outline-none focus:border-gray-600 w-full md:w-auto"
             />
-            <button className="absolute left-0 top-0 h-full flex items-center pl-2 text-gray-600">
+            <button 
+                type="button"
+                onClick={iconClick}
+                className="absolute left-0 top-0 h-full flex items-center pl-2 text-gray-600"
+            >
                 <FiSearch size={20}/>
             </button>
         </div>
