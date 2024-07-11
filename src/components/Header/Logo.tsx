835a80ee6434ce8e066cb5ed88e23f6bfb810731
Guilhermefariah <guilhermefaria321@gmail.com>
2024-07-11 14:12:29 -0300

    update logo

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Image from "next/image";
 
-
 const Logo: React.FC = () => {
     return (
         <div className="flex items-center">
-            <Image src="/img/logo.png" alt="Acidente Zero" width={50} height={50} />
+            <Image 
+                src="/img/logo.png" 
+                alt="Acidente Zero" 
+                width={50} 
+                height={50} 
+                className="w-10 h-10 sm:w-12 sm:h-12 md:w-16 md:h-16 lg:w-20 lg:h-20"
+            />
         </div>
     );
 }
