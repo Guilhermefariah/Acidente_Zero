75e7691b1e27eb6692ae7aaa0d2aa340dcbdc588
Guilhermefariah <guilhermefaria321@gmail.com>
2024-07-11 11:53:18 -0300

    width search

--- src/components/Header/Search.tsx
+++ src/components/Header/Search.tsx
@@ -7,7 +7,7 @@ const Search: React.FC = () => {
             <input 
                 type="text"
                 placeholder="Pesquisar..."
-                className="px-3 py-2 pl-10 border border-gray-200 font-serif rounded-md focus:outline-none focus:border-gray-600"
+                className="px-3 py-2 pl-10 border border-gray-200 font-serif rounded-md focus:outline-none focus:border-gray-600 w-full md:w-auto"
             />
             <button className="absolute left-0 top-0 h-full flex items-center pl-2 text-gray-600">
                 <FiSearch size={20}/>
