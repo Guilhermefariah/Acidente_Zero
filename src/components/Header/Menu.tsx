14353cc4be501db0c75586e4661468af875630f4
Guilhermefariah <guilhermefaria321@gmail.com>
2024-07-11 11:29:41 -0300

    estado de accordion

--- src/components/Header/Menu.tsx
+++ src/components/Header/Menu.tsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import Link from "next/link";
+import { FiGithub, FiLinkedin } from "react-icons/fi";
 
 const Menu: React.FC = () => {
+    
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    }
+
     return (
         <div className="flex items-center justify-center md:jus space-x-4">
             {/* Itens do menu */}
