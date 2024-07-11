449ea8bf44327d3a344cbcff64d566740f172558
Guilhermefariah <guilhermefaria321@gmail.com>
2024-07-11 17:38:25 -0300

    update text-xl

--- src/components/Header/Navigation.tsx
+++ src/components/Header/Navigation.tsx
@@ -6,16 +6,16 @@ const Navigation: React.FC = () => {
         <nav className="flex flex-col md:flex-row items-center mt-2 md:mt-0">
             <ul className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-4">
                 <li>
-                    <Link href="/" className="text-gray-600 font-serif hover:text-gray-900">Início</Link>
+                    <Link href="/" className="text-gray-600 font-serif hover:text-gray-900 text-xl">Início</Link>
                 </li>
                 <li>
-                    <Link href="/about" className="text-gray-600 font-serif hover:text-gray-900">Sobre</Link>
+                    <Link href="/about" className="text-gray-600 font-serif hover:text-gray-900 text-xl">Sobre</Link>
                 </li>
                 <li>
-                    <Link href="/report" className="text-gray-600 font-serif hover:text-gray-900">Relatar</Link>
+                    <Link href="/report" className="text-gray-600 font-serif hover:text-gray-900 text-xl">Relatar</Link>
                 </li>
                 <li>
-                    <Link href="/contact" className="text-gray-600 font-serif hover:text-gray-900">Contato</Link>
+                    <Link href="/contact" className="text-gray-600 font-serif hover:text-gray-900 text-xl">Contato</Link>
                 </li>
             </ul>
         </nav>
--- src/components/Header/Search.tsx
+++ src/components/Header/Search.tsx
@@ -16,7 +16,7 @@ const Search: React.FC = () => {
                 type="text"
                 placeholder="Pesquisar..."
                 ref={inputRef}
-                className="px-2 py-2 pl-10 border text-gray-600 border-gray-200 font-serif rounded-md focus:outline-none focus:border-gray-400 w-full md:w-auto"
+                className="px-2 py-2 pl-10 border text-xl text-gray-600 border-gray-200 font-serif rounded-md focus:outline-none focus:border-gray-400 w-full md:w-auto"
             />
             <button 
                 type="button"
