6066d84c8a76120d658983ea9046f044ab5f751d
Guilhermefariah <guilhermefaria321@gmail.com>
2024-07-12 11:14:07 -0300

    MapContainer adicionado

---
+++ src/components/Map/Map.tsx
@@ -0,0 +1,12 @@
+import { MapContainer } from "react-leaflet";
+
+
+const Map = () => {
+    return (
+        <MapContainer center={[-23.257046, -46.739705]} zoom={13} style={{ height: "500px", width: "100%" }} >
+            
+        </MapContainer>
+    )
+}
+
+export default Map;
