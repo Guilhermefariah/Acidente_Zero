01dae1d67eefa4a4c1097ec1c33831f397735847
Guilhermefariah <guilhermefaria321@gmail.com>
2024-07-11 21:41:56 -0300

    update header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,25 +7,24 @@ import Navigation from "./Navigation";
 import PageTitle from "./PageTitle";
 
 const Header: React.FC = () => {
+  const pageTitle = "Acidente Zero";
 
-    const pageTitle = "Acidente Zero";
-
-    return (
-        <header className="bg-gray-200 shadow-lg">
-        <div className="container mx-auto py-4 px-4 md:px-0 flex flex-col md:flex-row justify-between items-center">
-          <div className="flex items-center space-x-4">
-            <Logo />
-            <PageTitle title={pageTitle} />
-          </div>
-          <Navigation />
-          <div className="flex items-center space-x-4 mt-4 md:mt-0">
-            <Search />
-            <UserMenu />
-          </div>
+  return (
+    <header className="bg-gray-200 shadow-lg">
+      <Navigation />
+      <div className="container py-2 px-4 md:px-0 flex flex-col md:flex-row justify-between items-center">
+        <div className="flex items-center space-x-4">
+          <Logo />
+          <PageTitle title={pageTitle} />
         </div>
-        <Menu />
-      </header>
-    );
+        <div className="flex items-center space-x-4 mt-2 md:mt-0">
+          <Search />
+          <UserMenu />
+        </div>
+      </div>
+      <Menu />
+    </header>
+  );
 }
 
 export default Header;
