bdf7722d7a7793997b1ea7defa98fd91077cf6a8
Guilhermefariah <guilhermefaria321@gmail.com>
2024-07-12 00:47:16 -0300

    Image in userMeu

--- src/components/Header/UserMenu.tsx
+++ src/components/Header/UserMenu.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import Image from "next/image";
 
 const UserMenu: React.FC = () => {
     return (
-        <div className="flex items-center space-x-4">
-            {/* Itens do menu do usuário */}
+        <div className="relative flex items-center space-x-4">
+            <div className="relative w-10 h-10 md:w-12 md:h-12">
+                <Image
+                    src="/img/avatar.png"
+                    alt="User Avatar"
+                    layout="fill"
+                    objectFit="cover"
+                    className="rounded-full"
+                />
+            </div>
         </div>
     );
 }
