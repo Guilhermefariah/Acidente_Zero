f8d0a420895432e1d346895b9589f6a19c0e44a7
Guilhermefariah <guilhermefaria321@gmail.com>
2024-07-11 21:59:51 -0300

    UI underline header

--- src/components/Header/Menu.tsx
+++ src/components/Header/Menu.tsx
@@ -12,14 +12,14 @@ const Menu: React.FC = () => {
         <div className="w-full">
             <button
                 onClick={toggleMenu}
-                className="w-full flex justify-between items-center bg-gray-200 px-4 py-2 text-left text-xl text-gray-600 font-serif hover:text-gray-900 focus:outline-none focus:bg-gray-300 md:hidden"
+                className="w-full flex justify-between items-center bg-gray-200 px-4 py-2 text-left text-xl text-gray-600 font-serif hover:text-gray-900 focus:outline-none focus:bg-gray-300 md:hidden hover:underline"
             >
                 Menu
             </button>
             <div className={`flex-col md:flex md:flex-row md:justify-between items-center bg-gray-100 md:bg-transparent space-y-4 md:space-y-0 md:space-x-4 py-4 md:py-0 ${isOpen ? 'flex' : 'hidden'}`}>
-                <Link href="/Novidades" className="block text-gray-600 font-serif hover:text-gray-900 text-xl">Novidades</Link>
-                <Link href="/Eventos" className="block text-gray-600 font-serif hover:text-gray-900 text-xl">Eventos</Link>
-                <Link href="/Apoio" className="block text-gray-600 font-serif hover:text-gray-900 text-xl">Apoio</Link>
+                <Link href="/Novidades" className="block text-gray-600 font-serif hover:text-gray-900 text-xl hover:underline">Novidades</Link>
+                <Link href="/Eventos" className="block text-gray-600 font-serif hover:text-gray-900 text-xl hover:underline">Eventos</Link>
+                <Link href="/Apoio" className="block text-gray-600 font-serif hover:text-gray-900 text-xl hover:underline">Apoio</Link>
                 <div className="flex space-x-4">
                     <a href="https://github.com/Guilhermefariah" target="_blank" rel="noopener noreferrer" className="text-gray-600 hover:text-gray-900">
                         <FiGithub size={24} />
