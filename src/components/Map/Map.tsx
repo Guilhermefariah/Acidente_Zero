4ae7b831a9257f00507bfb6c4bbcee0851df7fe6
Guilhermefariah <guilhermefaria321@gmail.com>
2024-07-12 15:24:57 -0300

    design Map

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from "react-leaflet";
 import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 
 interface MarkerType {
     position: L.LatLng;
@@ -39,23 +40,29 @@ const Map = () => {
     });
 
     return (
-        <div>
-            <MapContainer center={[-23.257046, -46.739705]} zoom={13} style={{ height: "500px", width: "100%" }}>
-                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>Acidente Zero</a> contributors"
-                />
-                <AddMarker />
-                {markers.map(marker => (
-                    <Marker key={marker.id} position={marker.position} icon={markerIcon}>
-                        <Popup>
-                            Problema relatado.
-                        </Popup>
-                    </Marker>
-                ))}
-            </MapContainer>
-            <div>
-                <button onClick={clearMarkers}>Limpar</button>
-                <button onClick={saveMarkers}>Salvar</button>
+        <div className="bg-gray-100 p-4">
+            <div className="border-2 border-gray-500">
+                <MapContainer center={[-23.257046, -46.739705]} zoom={13} className="h-80 sm:h-96 md:h-[500px] w-full">
+                    <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>Acidente Zero</a> contributors"
+                    />
+                    <AddMarker />
+                    {markers.map(marker => (
+                        <Marker key={marker.id} position={marker.position} icon={markerIcon}>
+                            <Popup>
+                                Problema relatado.
+                            </Popup>
+                        </Marker>
+                    ))}
+                </MapContainer>
+            </div>
+            <div className="text-start mt-4">
+                <button className="bg-gray-700 text-white py-2 px-4 rounded hover:bg-gray-800 transform transition-transform duration-300 ease-in-out hover:scale-110" onClick={clearMarkers}>
+                    Limpar
+                </button>
+                <button className="bg-gray-500 text-white py-2 px-4 rounded ml-2 hover:bg-gray-600 transform transition-transform duration-300 ease-in-out hover:scale-110" onClick={saveMarkers}>
+                    Salvar
+                </button>
             </div>
         </div>
     )
