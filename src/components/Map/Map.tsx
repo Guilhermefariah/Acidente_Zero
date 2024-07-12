7f27f62520d18c49952810bdf9e6ed5357cdc821
Guilhermefariah <guilhermefaria321@gmail.com>
2024-07-12 14:22:35 -0300

    update position button

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -39,23 +39,25 @@ const Map = () => {
     });
 
     return (
-        <MapContainer center={[-23.257046, -46.739705]} zoom={13} style={{ height: "500px", width: "100%" }}>
-            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>Acidente Zero</a> contributors"
-            />
-            <AddMarker />
+        <div>
+            <MapContainer center={[-23.257046, -46.739705]} zoom={13} style={{ height: "500px", width: "100%" }}>
+                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>Acidente Zero</a> contributors"
+                />
+                <AddMarker />
+                {markers.map(marker => (
+                    <Marker key={marker.id} position={marker.position} icon={markerIcon}>
+                        <Popup>
+                            Problema relatado.
+                        </Popup>
+                    </Marker>
+                ))}
+            </MapContainer>
             <div>
-                <button onClick={clearMarkers}>Limpar Marcadores</button>
-                <button onClick={saveMarkers}>Salvar Marcadores</button>
+                <button onClick={clearMarkers}>Limpar</button>
+                <button onClick={saveMarkers}>Salvar</button>
             </div>
-            {markers.map(marker => (
-                <Marker key={marker.id} position={marker.position} icon={markerIcon}>
-                    <Popup>
-                        Problema relatado.
-                    </Popup>
-                </Marker>
-            ))}
-        </MapContainer>
+        </div>
     )
 }
 
