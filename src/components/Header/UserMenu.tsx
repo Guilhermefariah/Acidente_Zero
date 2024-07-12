72cde0e886920f2b2394027cba93b85e168f163e
Guilhermefariah <guilhermefaria321@gmail.com>
2024-07-12 02:53:16 -0300

    funcao para abrir menu

--- src/components/Header/UserMenu.tsx
+++ src/components/Header/UserMenu.tsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import { FiBell, FiUser } from "react-icons/fi";
 
 const UserMenu: React.FC = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    }
+
     return (
         <div className="relative flex items-center space-x-4">
             <div className="relative w-10 h-10 md:w-12 md:h-12">
@@ -20,15 +26,17 @@ const UserMenu: React.FC = () => {
                 <span className="absolute top-0 right-0 inline-block w-2 h-2 bg-red-600 rounded-full"> </span>
             </button>
             <div className="relative flex items-center">
-                <button className="flex items-center space-x-2 focus:outline-none" aria-label="User Menu">
+                <button onClick={toggleMenu} className="flex items-center space-x-2 focus:outline-none" aria-label="User Menu">
                     <FiUser size={30} className="text-gray-600" />
                     <span className="hidden md:block">Perfil</span>
                 </button>
-                <div className="absolute right-0 mt-2 w-48 bg-gray-200 rounded-md shadow-lg py-0 flex flex-col">
-                    <a href="/profie" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">Perfil</a>
-                    <a href="/settings" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">Configuração</a>
-                    <button className="block w-full text-left px-4 py-2 text-gray-700 hover:bg-gray-100">Sair</button>
-                </div>
+                {menuOpen && (
+                    <div className="absolute right-0 mt-2 w-48 bg-gray-200 rounded-md shadow-lg py-0 flex flex-col">
+                        <a href="/profie" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">Perfil</a>
+                        <a href="/settings" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">Configuração</a>
+                        <button className="block w-full text-left px-4 py-2 text-gray-700 hover:bg-gray-100">Sair</button>
+                    </div>
+                )}
             </div>
         </div>
     );
