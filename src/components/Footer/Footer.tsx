8d4fa5329a6723ef92e5a53c3f9dcd3eb2fe9b88
Guilhermefariah <guilhermefaria321@gmail.com>
2024-07-12 16:09:46 -0300

    remove position absolute footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-gray-200 text-gray-900 text-center font-serif py-4 absolute bottom-0 w-full">
+    <footer className="bg-gray-200 text-gray-900 text-center font-serif py-2">
       <div className="container mx-auto">
-        <p className="text-xl ">&copy; {new Date().getFullYear()} Acidente Zero. Todos os direitos reservados.</p>
-        <p className="text-xl ">Contato: contato@acidente-zero.com</p>
+        <p className="text-xl">&copy; {new Date().getFullYear()} Acidente Zero. Todos os direitos reservados.</p>
+        <p className="text-xl">Contato: contato@acidente-zero.com</p>
       </div>
     </footer>
   );
