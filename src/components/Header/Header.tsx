c7190fa0ef43e5c054cf6007e30c690630e1c2e6
Guilhermefariah <guilhermefaria321@gmail.com>
2024-07-12 01:18:32 -0300

    Header update responsivo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -12,12 +12,12 @@ const Header: React.FC = () => {
   return (
     <header className="bg-gray-200 shadow-lg">
       <Navigation />
-      <div className="container py-2 px-4 md:px-0 flex flex-col md:flex-row justify-between items-center">
-        <div className="flex items-center space-x-4">
+      <div className="flex flex-col md:flex-row items-center justify-between px-4 py-2 space-y-2 md:space-y-0">
+        <div className="flex items-center space-x-2">
           <Logo />
           <PageTitle title={pageTitle} />
         </div>
-        <div className="flex items-center space-x-4 mt-2 md:mt-0">
+        <div className="flex flex-col md:flex-row items-center space-y-2 md:space-y-0 md:space-x-4">
           <Search />
           <UserMenu />
         </div>
