483e4afc21b4edc523f339de2450dc21b285ccff
Guilhermefariah <guilhermefaria321@gmail.com>
2024-07-12 03:30:32 -0300

    UserMenu remocao do icon

--- src/components/Header/UserMenu.tsx
+++ src/components/Header/UserMenu.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useRef, useEffect, } from "react";
-import Image from "next/image";
 import { FiBell, FiUser } from "react-icons/fi";
 
 const UserMenu: React.FC = () => {
@@ -12,7 +11,7 @@ const UserMenu: React.FC = () => {
                 setMenuOpen(false);
             }
         }
-        document.addEventListener('mousedown', clickOutside);
+        document.addEventListener("mousedown", clickOutside);
 
         return () => {
             document.removeEventListener("mousedown", clickOutside);
@@ -25,16 +24,6 @@ const UserMenu: React.FC = () => {
 
     return (
         <div className="relative flex items-center space-x-4">
-            <div className="relative w-10 h-10 md:w-12 md:h-12">
-                <Image
-                    src="/img/avatar.png"
-                    alt="User Avatar"
-                    layout="fill"
-                    objectFit="cover"
-                    className="rounded-full"
-                />
-            </div>
-            <span className="font-serif text-gray-600 text-base">Guilherme Faria</span>
             <button aria-label="Notification" className="relative">
                 <FiBell size={30} className="text-gray-600" />
                 <span className="absolute top-0 right-0 inline-block w-2 h-2 bg-red-600 rounded-full"> </span>
