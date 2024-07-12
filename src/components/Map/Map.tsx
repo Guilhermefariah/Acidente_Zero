9d216ebc8eb2d2bd7a227431c2ad455e680a812e
Guilhermefariah <guilhermefaria321@gmail.com>
2024-07-12 12:32:45 -0300

    add TileLayer

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,10 +1,12 @@
-import { MapContainer } from "react-leaflet";
+import { MapContainer, TileLayer } from "react-leaflet";
 
 
 const Map = () => {
     return (
         <MapContainer center={[-23.257046, -46.739705]} zoom={13} style={{ height: "500px", width: "100%" }} >
-            
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution="&copy;<a href='https://www.openstreetmap.org/copyright'>Acidente Zero</a> contributors"
+            />
         </MapContainer>
     )
 }
