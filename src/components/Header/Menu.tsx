3970d4952ffc9d01d696679a4aba7fcb3f56decf
Guilhermefariah <guilhermefaria321@gmail.com>
2024-07-13 21:18:29 -0300

    melhorando praticas para os links

--- src/components/Header/Menu.tsx
+++ src/components/Header/Menu.tsx
@@ -16,9 +16,11 @@ const Menu: React.FC = () => {
                 Menu
             </button>
             <div className={`flex-col md:flex md:flex-row md:justify-between items-start bg-gray-100 md:bg-transparent space-y-4 md:space-y-0 md:space-x-4 py-4 md:py-0 ${isOpen ? 'flex' : 'hidden'}`}>
-                <Link href="/Novidades" className="block text-gray-700 font-serif hover:text-gray-900 text-xl hover:underline">Novidades</Link>
-                <Link href="/Eventos" className="block text-gray-700 font-serif hover:text-gray-900 text-xl hover:underline">Eventos</Link>
-                <Link href="/Apoio" className="block text-gray-700 font-serif hover:text-gray-900 text-xl hover:underline">Apoio</Link>
+                {['Novidades', 'Eventos', 'Apoio'].map((item, index) => (
+                    <Link key={index} href={`/${item.toLowerCase()}`} className="block text-gray-700 font-serif hover:text-gray-900 text-xl hover:underline">
+                        {item}
+                    </Link>
+                ))}
             </div>
         </div>
     );
