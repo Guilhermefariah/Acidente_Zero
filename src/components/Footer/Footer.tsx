5d48ab890efc2b10283b5960e276fc1f29df9ece
Guilhermefariah <guilhermefaria321@gmail.com>
2024-07-13 21:01:44 -0300

    Adicionando constando currentYear

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 const Footer: React.FC = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className="bg-gray-200 text-gray-900 text-center font-serif py-2">
       <div className="container mx-auto">
-        <p className="text-xl">&copy; {new Date().getFullYear()} Acidente Zero. Todos os direitos reservados.</p>
-        <p className="text-xl">Contato: contato@acidente-zero.com</p>
+        <p className="text-xl">&copy; {currentYear} Acidente Zero. Todos os direitos reservados.</p>
+        <p className="text-xl">Contato: <a href="mailto:contato@acidente-zero.com" className="hover:underline">contato@acidente-zero.com</a></p>
       </div>
     </footer>
   );
