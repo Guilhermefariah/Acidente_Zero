8e714d12288766a4a0c9879fb0a946b2968c9e63
Guilhermefariah <guilhermefaria321@gmail.com>
2024-07-17 04:22:15 -0300

    footer bg-gradient-to-r

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ const Footer: React.FC = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="bg-gray-400 text-gray-800 text-center font-serif py-2">
+    <footer className="bg-gradient-to-r from-gray-100 via-gray-300 to-gray-500 text-center font-serif">
       <div className="container mx-auto">
         <p className="text-xl">&copy; {currentYear} Acidente Zero. Todos os direitos reservados.</p>
         <p className="text-xl">Contato: <a href="mailto:contato@acidente-zero.com" className="hover:underline">contato@acidente-zero.com</a></p>
