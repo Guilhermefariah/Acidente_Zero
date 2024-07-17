3eb1a9c28c446645fecda7ef9393849e63895686
Guilhermefariah <guilhermefaria321@gmail.com>
2024-07-17 11:33:41 -0300

    resolvendo o erro TypeScript.ReactNode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import React, { ReactNode } from "react";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { metadata } from "@/models/Metadata/metadata";
+
 const inter = Inter({ subsets: ["latin"] });
 
 interface RootLayoutProps {
@@ -9,12 +10,15 @@ interface RootLayoutProps {
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
-  const descriptionContent = metadata.description || "";
+  const title = (metadata.title as string) || "Default Title";
+  const descriptionContent = (metadata.description as string) || "Default description";
 
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <head>
-        <title>{String(metadata.title)}</title>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>{title}</title>
         <meta name="description" content={descriptionContent} />
       </head>
       <body className={inter.className}>{children}</body>
