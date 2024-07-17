ad93f904f62d0a9f5eb15c387295d0207774460d
Guilhermefariah <guilhermefaria321@gmail.com>
2024-07-17 04:12:57 -0300

    Correção: remoção API

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ const Footer: React.FC = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="bg-gray-200 text-gray-900 text-center font-serif py-2">
+    <footer className="bg-gray-400 text-gray-800 text-center font-serif py-2">
       <div className="container mx-auto">
         <p className="text-xl">&copy; {currentYear} Acidente Zero. Todos os direitos reservados.</p>
         <p className="text-xl">Contato: <a href="mailto:contato@acidente-zero.com" className="hover:underline">contato@acidente-zero.com</a></p>
