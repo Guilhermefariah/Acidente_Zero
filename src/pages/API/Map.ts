f704f2c43c0e7d7488d72aaa4595c2c322f21e51
Guilhermefariah <guilhermefaria321@gmail.com>
2024-07-17 03:19:05 -0300

    Revert "Comentario Map.ts"
    This reverts commit c1ee81b379a6adbd79373ac2b6c61bdf3d1ceeb1.

--- src/pages/API/Map.ts
+++ src/pages/API/Map.ts
@@ -1,26 +1,26 @@
-// import { NextApiRequest, NextApiResponse } from 'next';
-// import { MarkerType } from '@/models/Map/MarkerType';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { MarkerType } from '@/models/Map/MarkerType';
 
-// let markers: MarkerType[] = [];
+let markers: MarkerType[] = [];
 
-// export default function handler(req: NextApiRequest, res: NextApiResponse) {
-//     const { method } = req;
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+    const { method } = req;
 
-//     switch (method) {
-//         case 'GET':
-//             res.status(200).json(markers);
-//             break;
-//         case 'POST':
-//             const newMarker: MarkerType = req.body;
-//             markers.push(newMarker);
-//             res.status(201).json(newMarker);
-//             break;
-//         case 'DELETE':
-//             markers = [];
-//             res.status(204).end();
-//             break;
-//         default:
-//             res.setHeader('Allow', ['GET', 'POST', 'DELETE']);
-//             res.status(405).end(`Method ${method} Not Allowed`);
-//     }
-// }
+    switch (method) {
+        case 'GET':
+            res.status(200).json(markers);
+            break;
+        case 'POST':
+            const newMarker: MarkerType = req.body;
+            markers.push(newMarker);
+            res.status(201).json(newMarker);
+            break;
+        case 'DELETE':
+            markers = [];
+            res.status(204).end();
+            break;
+        default:
+            res.setHeader('Allow', ['GET', 'POST', 'DELETE']);
+            res.status(405).end(`Method ${method} Not Allowed`);
+    }
+}
