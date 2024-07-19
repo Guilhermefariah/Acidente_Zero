e6c9d873f329791414411f579b3699cdfb68be4e
Guilhermefariah <guilhermefaria321@gmail.com>
2024-07-19 15:15:34 -0300

    :rocket: melhorias de desempenho

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,9 +5,11 @@ const Footer: React.FC = () => {
 
   return (
     <footer className="bg-gradient-to-r from-gray-100 via-gray-300 to-gray-500 text-center font-serif">
-      <div className="container mx-auto">
-        <p className="text-xl">&copy; {currentYear} Acidente Zero. Todos os direitos reservados.</p>
-        <p className="text-xl">Contato: <a href="mailto:contato@acidente-zero.com" className="hover:underline">contato@acidente-zero.com</a></p>
+      <div className="container mx-auto py-4">
+        <>
+          <p className="text-xl">&copy; {currentYear} Acidente Zero. Todos os direitos reservados.</p>
+          <p className="text-xl">Contato: <a href="mailto:contato@acidente-zero.com" className="hover:underline">contato@acidente-zero.com</a></p>
+        </>
       </div>
     </footer>
   );
