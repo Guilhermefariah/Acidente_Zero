9790724f2aeb119dc9dc9fbd6c4ac0bdbbb11f47
Guilhermefariah <guilhermefaria321@gmail.com>
2024-07-19 15:12:07 -0300

    :rocket: melhorias de desempenho

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,19 @@
 'use client';
-import Head from "next/head";
+import React from 'react';
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
 import dynamic from 'next/dynamic';
-
 
 const Map = dynamic(() => import('@/components/Map/Map'), { ssr: false });
 
 export default function Home() {
     return (
-        <div className="flex flex-col min-h-screen">
-            <Head>
-                <title>Acidente Zero</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
+        <>
             <Header />
             <main className="flex-grow">
                 <Map />
             </main>
             <Footer />
-        </div>
+        </>
     );
 }
