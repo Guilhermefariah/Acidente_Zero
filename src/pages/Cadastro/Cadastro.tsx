5c15e56f788ef4e85b9dafd2d3e88c3ebf607d73
Guilhermefariah <guilhermefaria321@gmail.com>
2024-07-20 19:12:45 -0300

    :sparkles: Update animation Cadastro.tsx

--- src/pages/Cadastro/Cadastro.tsx
+++ src/pages/Cadastro/Cadastro.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { motion } from 'framer-motion';
 import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer/Footer';
 import '@/app/globals.css';
@@ -28,9 +29,26 @@ const Cadastro: React.FC = () => {
         <div className="flex flex-col min-h-screen">
             <Header />
             <main className="flex-grow container mx-auto p-4">
-                <form onSubmit={submitForm} className="bg-gradient-to-r from-gray-100 via-gray-300 to-gray-500 p-6 space-y-4 rounded-md shadow-md">
-                    <h1 className="text-gray-700 text-4xl font-serif text-center mb-6">Cadastro</h1>
-                    <div>
+                <motion.form 
+                    onSubmit={submitForm} 
+                    className="bg-gradient-to-r from-gray-100 via-gray-300 to-gray-500 p-6 space-y-4 rounded-md shadow-md"
+                    initial={{ opacity: 0, y: 50 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.5 }}
+                >
+                    <motion.h1 
+                        className="text-gray-700 text-4xl font-serif text-center mb-6"
+                        initial={{ opacity: 0, y: -50 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 0.5 }}
+                    >
+                        Cadastro
+                    </motion.h1>
+                    <motion.div
+                        initial={{ opacity: 0, x: -50 }}
+                        animate={{ opacity: 1, x: 0 }}
+                        transition={{ duration: 0.5, delay: 0.3 }}
+                    >
                         <label htmlFor="name" className="block text-2xl font-serif text-gray-700">Nome</label>
                         <input
                             type="text"
@@ -41,8 +59,12 @@ const Cadastro: React.FC = () => {
                             onChange={setValue}
                             className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-gray-400"
                         />
-                    </div>
-                    <div>
+                    </motion.div>
+                    <motion.div
+                        initial={{ opacity: 0, x: 50 }}
+                        animate={{ opacity: 1, x: 0 }}
+                        transition={{ duration: 0.5, delay: 0.6 }}
+                    >
                         <label htmlFor="email" className="block text-2xl font-serif text-gray-700">Email</label>
                         <input
                             type="email"
@@ -53,8 +75,12 @@ const Cadastro: React.FC = () => {
                             onChange={setValue}
                             className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-gray-400"
                         />
-                    </div>
-                    <div>
+                    </motion.div>
+                    <motion.div
+                        initial={{ opacity: 0, x: -50 }}
+                        animate={{ opacity: 1, x: 0 }}
+                        transition={{ duration: 0.5, delay: 0.9 }}
+                    >
                         <label htmlFor="password" className="block text-2xl font-serif text-gray-700">Senha</label>
                         <input
                             type="password"
@@ -65,9 +91,17 @@ const Cadastro: React.FC = () => {
                             onChange={setValue}
                             className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-gray-400"
                         />
-                    </div>
-                    <button type="submit" className="bg-blue-600 text-gray-100 hover:text-gray-200 text-2xl py-2 px-4 rounded-md hover:bg-blue-700 transition duration-200 transform hover:scale-105 w-full">Cadastrar</button>
-                </form>
+                    </motion.div>
+                    <motion.button 
+                        type="submit" 
+                        className="bg-blue-600 text-gray-100 hover:text-gray-200 text-2xl py-2 px-4 rounded-md hover:bg-blue-700 transition duration-200 transform hover:scale-105 w-full"
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        transition={{ duration: 0.5, delay: 1.2 }}
+                    >
+                        Cadastrar
+                    </motion.button>
+                </motion.form>
             </main>
             <Footer />
         </div>
