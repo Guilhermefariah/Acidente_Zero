ba6c05e478ccbf30d502df5fc8f74b17657c327f
Guilhermefariah <guilhermefaria321@gmail.com>
2024-09-07 05:26:10 -0300

    atualizando links footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 export const Footer: React.FC = () => {
@@ -7,7 +8,7 @@ export const Footer: React.FC = () => {
     <footer className='bg-gray-800 text-center text-white font-serif py-4'>
       <div className='container mx-auto'>
         <p className='text-xl'>&copy; {currentYear} Acidente Zero. Todos os direitos reservados. </p>
-        <p className='text-xl'>Contato: <a href='mailto:contato@acidente-zero.com' className='hover:underline'>contato@acidente-zero.com</a></p>
+        <p className='text-xl'>Contato: <Link href='mailto:contato@acidente-zero.com' className='hover:underline'>contato@acidente-zero.com</Link></p>
       </div>
     </footer>
   )
