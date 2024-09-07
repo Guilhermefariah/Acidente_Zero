12aacc10447925d2c36c1876131a9dbd59b5806b
Guilhermefariah <guilhermefaria321@gmail.com>
2024-09-07 12:32:56 -0300

    updated design

--- src/components/Header/Search.tsx
+++ src/components/Header/Search.tsx
@@ -16,12 +16,12 @@ export const Search: React.FC = () => {
                 type='text'
                 placeholder='Pesquisar...'
                 ref={inputRef}
-                className='px-2 py-2 pl-10 border text-xl text-gray-200 border-gray-700 bg-gray-900 rounded-md focus:outline-none focus:border-gray-600 w-full md:w-auto'
+                className='px-2 py-2 pl-10 border text-xl text-gray-300 border-gray-700 bg-gray-900 rounded-md focus:outline-none focus:border-gray-600 w-full md:w-auto'
             />
             <button
                 type='button'
                 onClick={iconClick}
-                className='absolute left-0 top-0 h-full flex items-center pl-2 text-gray-200'
+                className='absolute left-0 top-0 h-full flex items-center pl-2 text-gray-300'
             >
                 <Image src='/img/search.png' alt='Search' width={25} height={25} />
             </button>
