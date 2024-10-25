b32b110c9439b41c01b062021adcd0530be2aa86
Guilhermefariah <guilhermefaria321@gmail.com>
2024-10-24 21:40:27 -0300

    feat: adicionado variants em pageTile

--- src/components/ui/PageTitle.tsx
+++ src/components/ui/PageTitle.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { motion } from 'framer-motion'
 
+const pageTitleClass = {
+    title: 'text-3xl sm:text-3xl md:text-4xl lg:text-5xl text-gray-300 font-serif ml-4'
+}
+
 interface PageTitleProps {
     title: string
 }
@@ -8,7 +12,7 @@ interface PageTitleProps {
 export const PageTitle: React.FC<PageTitleProps> = ({ title }) => {
     return (
         <motion.h1
-            className='text-3xl sm:text-3xl md:text-4xl lg:text-5xl text-gray-300 font-serif ml-4'
+            className={pageTitleClass.title}
             initial={{ opacity: 0, y: -20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.3 }}
