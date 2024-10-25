a507cecda2b05c688fb391ea4208678b245f49ff
Guilhermefariah <guilhermefaria321@gmail.com>
2024-10-24 21:07:03 -0300

    feat: adicionado variants em Logo

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import Image from 'next/image'
 
+const logoClass = {
+    image: 'w-12 h-12 sm:w-14 sm:h-14 md:w-16 md:h-16 lg:w-20 lg:h-20'
+}
+
 export const Logo: React.FC = () => {
     return (
         <Image
@@ -8,7 +12,7 @@ export const Logo: React.FC = () => {
             alt='Logo'
             width={20}
             height={20}
-            className='w-12 h-12 sm:w-14 sm:h-14 md:w-16 md:h-16 lg:w-20 lg:h-20'
+            className={logoClass.image}
         />
     )
 }
